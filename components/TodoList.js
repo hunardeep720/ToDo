1e948f1fb6050ff2ba31ae9d9ab9718a9f2ec31b
hunardeep720 <123598156+hunardeep720@users.noreply.github.com>
2024-03-31 00:33:57 -0600

    lab5

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,28 +1,28 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { ScrollView, View, Text } from "react-native";
-import {styles} from "../layouts/MainLayout";
+import { styles } from "../layouts/MainLayout";
 import ToDoForm from "./ToDoForm";
 
-const ToDoList = () => {
-  let [list, setList] = useState(["Do laundry", "Go to gym", "Walk dog"]);
-  let [list1, setList1] = useState(["DO LAUNDRY", "GO TO GYM", "WALK DOG"]);
-  const addTask = (task) => {
-    if(!list1.includes(task.toUpperCase())){
-      setList1([...list1, task.toUpperCase()]);
-      setList([...list, task]);
+const TaskList = () => {
+  let [taskList, setTaskList] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+  let [taskListUpper, setTaskListUpper] = useState(taskList.map((task) => task.toUpperCase()));
+  const addNewTask = (task) => {
+    if(!taskListUpper.includes(task.toUpperCase())){
+      setTaskListUpper([...taskListUpper, task.toUpperCase()]);
+      setTaskList([...taskList, task]);
     }
   }
   return (
     <ScrollView>
       <View>
-        {list.map((l1, index) => (
+        {taskList.map((task, index) => (
           <Text key={index} style={[styles.taskText, styles.marginY,styles.textColor]}>
-            &#8226; {l1}
+            &#8226; {task}
           </Text>
         ))}
-        <ToDoForm style={[styles.center,{marginTop:50}]} addTask={addTask} />
+        <ToDoForm style={[styles.center,{marginTop:50}]} addTask={addNewTask} />
       </View>
     </ScrollView>
   );
 };
-export default ToDoList;
+export default TaskList;
